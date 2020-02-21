527120715e68514ff72074b6061e5a8cc69ed872
JMakim <jmakimramjahn@gmail.com>
2020-02-21 18:13:56 -0500

    Milestone 1
    done

---
+++ Milestone1/Multiplayer game/Assets/Script/Box.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    public bool active = true;
+
+    public GameObject Cross;
+    public GameObject Circle;
+
+    public int Check;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetObjects();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (GetComponentInParent<Game>().activeGame == true)
+            {
+
+
+                if (active == true)
+                {
+                    if (GetComponentInParent<Game>().Player1 == true)
+                    {
+                        Cross.SetActive(true);
+                        GetComponentInParent<Game>().Player1 = false;
+                        GetComponentInParent<Game>().Player2 = true;
+                        Check = 1;
+                    }
+                    else
+                    {
+                        Circle.SetActive(true);
+                        GetComponentInParent<Game>().Player1 = true;
+                        GetComponentInParent<Game>().Player2 = false;
+                        Check = 2;
+                    }
+
+                    active = false;
+                }
+            }
+        }
+    }
+
+    void GetObjects()
+    {
+        Cross = this.transform.Find("Cross").gameObject;
+        Circle = this.transform.Find("Circle").gameObject;
+    }
+}
---
+++ Milestone1/Multiplayer game/Assets/Script/Game.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+
+    string PlayerOneName;
+
+
+
+
+
+
+    public bool activeGame = true;
+
+    public bool Player1 = true;
+    public bool Player2 = false;
+
+    public int whoWin;
+
+    public int one;
+    public int two;
+    public int three;
+    public int four;
+    public int five;
+    public int six;
+    public int seven;
+    public int eight;
+    public int nine;
+
+    public GameObject UI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetUp();
+        WinCondition();
+
+        if (whoWin == 1)
+        {
+            UI.transform.GetChild(2).gameObject.SetActive(true);
+        }
+        if (whoWin == 2)
+        {
+            UI.transform.GetChild(3).gameObject.SetActive(true);
+            Debug.Log("Player2 win");
+        }
+    }
+
+  
+
+    void SetUp()
+    {
+        one = this.transform.Find("Play Area 1").gameObject.GetComponent<Box>().Check;
+        two = this.transform.Find("Play Area 2").gameObject.GetComponent<Box>().Check;
+        three = this.transform.Find("Play Area 3").gameObject.GetComponent<Box>().Check;
+        four = this.transform.Find("Play Area 4").gameObject.GetComponent<Box>().Check;
+        five = this.transform.Find("Play Area 5").gameObject.GetComponent<Box>().Check;
+        six = this.transform.Find("Play Area 6").gameObject.GetComponent<Box>().Check;
+        seven = this.transform.Find("Play Area 7").gameObject.GetComponent<Box>().Check;
+        eight = this.transform.Find("Play Area 8").gameObject.GetComponent<Box>().Check;
+        nine = this.transform.Find("Play Area 9").gameObject.GetComponent<Box>().Check;
+        UI = GameObject.Find("Canvas");
+    }
+    void WinCondition()
+    {
+        if( one == 1 && two == 1 && three == 1)
+        {
+            activeGame = false;
+            whoWin = 1;
+
+        }
+        if( four == 1 && five == 1 && six == 1)
+        { 
+            activeGame = false; 
+            whoWin = 1;
+
+        }
+        if( seven == 1 && eight == 1 && nine == 1)
+        { 
+            activeGame = false;
+            whoWin = 1;
+
+        }
+        if( one == 1 && four == 1 && seven == 1)
+        { 
+            activeGame = false;
+            whoWin = 1;
+
+        }
+        if( two == 1 && five == 1 && eight == 1)
+        {
+            whoWin = 1;
+            activeGame = false;
+
+        }
+        if( three == 1 && six == 1 && nine == 1)
+        {
+            whoWin = 1;
+            activeGame = false;
+        }
+        if( one == 1 && five == 1 && nine == 1)
+        {
+            whoWin = 1;
+            activeGame = false;
+
+        }
+        if( seven == 1 && five == 1 && three == 1)
+        {
+            whoWin = 1;
+            activeGame = false;
+
+        }
+ //---------------------------------------------------------------------
+        if (one == 2 && two == 2 && three == 2)
+        {
+            whoWin = 2;
+            activeGame = false;
+
+        }
+        if (four == 2 && five == 2 && six == 2)
+        {
+            whoWin = 2;
+            activeGame = false;
+
+        }
+        if (seven == 2 && eight == 2 && nine == 2)
+        {
+            whoWin = 2;
+            activeGame = false;
+
+        }
+        if (one == 2 && four == 2 && seven == 2)
+        {
+            whoWin = 2;
+            activeGame = false;
+
+        }
+        if (two == 2 && five == 2 && eight == 2)
+        {
+            whoWin = 2;
+            activeGame = false;
+
+        }
+        if (three == 2 && six == 2 && nine == 2)
+        {
+            whoWin = 2;
+            activeGame = false;
+
+        }
+        if (one == 2 && five == 2 && nine == 2)
+        {
+            whoWin = 2;
+            activeGame = false;
+
+        }
+        if (seven == 2 && five == 2 && three == 2)
+        {
+            whoWin = 2;
+            activeGame = false;
+
+        }
+    }
+
+ 
+
+}
